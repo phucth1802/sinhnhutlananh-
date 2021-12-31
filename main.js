08ee00e94aa85a1d2b16bdcd4295e734cba6c8ab
phucth1802 <88522862+phucth1802@users.noreply.github.com>
2021-12-31 23:55:46 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+(function($){
+    "usr strict"
+    if($('.typed-text-output').length == 1){
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output',{
+            strings:typed_strings.split(','),
+            typeSpeed:100,
+            backSpeed:20,
+            smartBackspace:false,
+            loop:true
+        });
+    }
+    $('.email').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-paper-plane').addClass("next");
+    } else {
+      $('.icon-paper-plane').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.email').click(
+  function(){
+    console.log("Something");
+    $('.email-section').addClass("fold-up");
+    $('.password-section').removeClass("folded");
+  }
+);
+
+$('.password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-lock').addClass("next");
+    } else {
+      $('.icon-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.password').click(
+  function(){
+    console.log("Something");
+    $('.password-section').addClass("fold-up");
+    $('.repeat-password-section').removeClass("folded");
+  }
+);
+
+$('.repeat-password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-repeat-lock').addClass("next");
+    } else {
+      $('.icon-repeat-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button.repeat-password').click(
+  function(){
+    console.log("Something");
+    $('.repeat-password-section').addClass("fold-up");
+    $('.success').css("marginTop", 0);
+  }
+);
+})(jQuery);
